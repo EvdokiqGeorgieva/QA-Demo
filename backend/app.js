6977d6a749830d73ca45e46b3824ee446855e438
Eva <evdokia.georgieva@limechain.tech>
2025-01-02 16:51:59 +0200

    latest updates

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
